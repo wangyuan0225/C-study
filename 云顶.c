e027c533cf48230bdad63fa6317bf22952bff54c
王渊 <2682220169@qq.com>
2022-10-10 17:39:03 +0800

    Create 云顶.c

---
+++ 云顶.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+
+
+//int main()
+//{
+//	int i, j, k;
+//	for (i = 1; i < 5; i++)
+//		{
+//			/* 观察每行的空格数量，补全循环条件 */
+//			for (j = i; j < 5; j++)
+//			{
+//				printf(" ");    //输出空格
+//			}
+//			/* 观察每行*号的数量，补全循环条件 */
+//			for (k = 0; k < i * 2 - 1; k++)
+//			{
+//				printf("*");   //每行输出的*号
+//			}
+//			printf("\n");     //每次循环换行
+//		}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//    /* 小伙伴们：
+//           选择你们认为最合理的循环结构完成功能吧 */
+//    int sum = 0;  //定义计算结果变量sum
+//    int i = 1;    //定义循环数字变量i
+//    int flag = 1; //定义符号状态变量flag
+//
+//    //使用while循环
+//  //  while (i <= 100)
+//  //  {
+//  //      i++;
+//		//flag = -flag;
+//  //      sum += flag * i;
+//  //  }
+//
+//    i = 1;  //重新初始化变量i
+//
+//    //do-while循环
+//  //  do {
+//  //      i++;
+//  //      flag = -flag;
+//		//sum += flag * i;
+//  //  } while (i <= 100);
+//
+//    i = 1;  //重新初始化变量i
+//
+//    //使用for循环
+//    for (i = 1; i <= 100; i++)
+//    {
+//		flag = -flag;
+//		sum += flag * i;
+//        
+//    }
+//    printf("sum=%d\n", -sum);
+//
+//    return 0;
+//}
+
+
+//int main()
+//{
+//	char c = 'a';
+//	int n = c;        //将c赋值给n
+//	float f = c;      //将c赋值给f
+//	double d = c;    //将c赋值给d
+//	printf("%d\n", n);
+//	printf("%f\n", f);
+//	printf("%lf\n", d);
+//	return 0;
+//}
