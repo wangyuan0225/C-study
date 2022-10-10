6383408d64fe140949a388aeb5b164052774d8e6
王渊 <2682220169@qq.com>
2022-10-10 23:07:52 +0800

    更新

--- 云顶.c
+++ 云顶.c
@@ -1,6 +1,72 @@
 #include <stdio.h>
 
+int main()
+{
+    /* 定义需要计算的日期 */
+    int year = 2008;
+    int month = 8;
+    int day = 8;
+    /*
+     * 请使用switch语句，if...else语句完成本题
+     * 如有想看小编思路的，可以点击左侧任务中的“不会了怎么办”
+     * 小编还是希望大家独立完成哦~
+     */
+    int days = 0;
+    switch(month)
+    {
+    case 12:
+        days += 31;
+    case 11:
+        days += 30;
+    case 10:
+        days += 31;
+    case 9:
+        days += 30;
+    case 8:
+        days += 31;
+    case 7:
+        days += 31;
+    case 6:
+        days += 30;
+    case 5:
+        days += 31;
+    case 4:
+        days += 30;
+    case 3:
+        days += 31;
+    case 2:
+        if (year % 4 == 0)
+        {
+            days += 29;
+        }
+        else
+        {
+            days += 28;
+        }
+    case 1:
+        days += 31;
+    }
+    printf("%d", days + day);
+    return 0;
+}
 
+
+//int main()
+//{
+//	int i, j;
+//	for (i = 9; i >= 1; i--)
+//	{
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf("%d*%d=%d ", i, j, j * i);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+
+//打印正三角形
 //int main()
 //{
 //	int i, j, k;
